1b93d896fcc54cd6bead01a4114e3730cbf0b81f
Fatih YİĞİT <fatih.yigit.35@gmail.com>
2025-04-17 15:43:04 +0300

    contexts added

---
+++ Infrastructure/CarBookApp.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,35 @@
+﻿using CarBookApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApp.Persistence.Context
+{
+    public class CarBookContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Server=db16381.public.databaseasp.net; Database=db16381; User Id=db16381; Password=9o+CY_7z5f!P; Encrypt=False; MultipleActiveResultSets=True; ");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAddress> FooterAddress { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
