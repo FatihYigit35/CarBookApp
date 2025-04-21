68c8f95e14cab6b9b38a80ebe7424b050ff955b4
Fatih YİĞİT <fatih.yigit.35@gmail.com>
2025-04-21 16:21:54 +0300

    repository class added

---
+++ Infrastructure/CarBookApp.Persistence/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using CarBookApp.Application.Interfaces;
+using CarBookApp.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApp.Persistence.Repositories
+{
+    public class Repository<T>(CarBookContext context) : IRepository<T> where T : class
+    {
+        private readonly CarBookContext _context = context;
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id).AsTask();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
