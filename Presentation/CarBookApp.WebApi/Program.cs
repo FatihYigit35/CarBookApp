28ca37adf55c13d732f4e94415ebaa62e1121c0c
Fatih YİĞİT <fatih.yigit.35@gmail.com>
2025-04-24 17:17:53 +0300

    added scoped

--- Presentation/CarBookApp.WebApi/Program.cs
+++ Presentation/CarBookApp.WebApi/Program.cs
@@ -1,6 +1,20 @@
+using CarBookApp.Application.Features.CQRS.Handlers.AboutHandlers.Read;
+using CarBookApp.Application.Features.CQRS.Handlers.AboutHandlers.Write;
+using CarBookApp.Application.Interfaces;
+using CarBookApp.Persistence.Context;
+using CarBookApp.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHansler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
