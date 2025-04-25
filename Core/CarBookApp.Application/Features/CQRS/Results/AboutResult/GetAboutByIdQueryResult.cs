65ebb3ca9c5ab52034ecff9e4af0db718585cf2a
Fatih YİĞİT <fatih.yigit.35@gmail.com>
2025-04-25 17:23:56 +0300

    code duplication cleared

--- Core/CarBookApp.Application/Features/CQRS/Results/AboutResult/GetAboutByIdQueryResult.cs
+++ Core/CarBookApp.Application/Features/CQRS/Results/AboutResult/GetAboutByIdQueryResult.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarBookApp.Application.Features.CQRS.Results.AboutResult
 {
-    public class GetAboutByIdQueryResult
+    public class GetAboutByIdQueryResult:GetAboutQueryResult
     {
-        public int Id { get; set; }
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-        public required string ImageUrl { get; set; }
     }
 }
